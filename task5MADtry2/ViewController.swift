d4366439e4c6fd6815ba753cfe3e54e845637abd
Daniil <krivelevdaniil@gmail.com>
2021-05-13 14:20:13 +0700

    Initial Commit

---
+++ task5MADtry2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task5MADtry2
+//
+//  Created by KriDan on 13.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
